814a5e5a9735af227a80f40da71d50720b77bb47
BulkSMS4 <xportbox@gmail.com>
2025-10-25 06:45:07 +0000

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // ---------- ENV (keep exactly what you provided) ----------
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || "Forgetme";
 const WHATSAPP_TOKEN = process.env.WHATSAPP_TOKEN || "EAAbnZCZA0lZBioBPZC1Bl4LKGbmeamRE9s5NZC9BUzxfX1f4agMEZBIYvMX04Wv8C5K0ZBvkg78azsQInnIZAWAFq7SQzfSgtRIBheqXobkC73i3aYWfQH6z70Mq8uhoBjOvlzgdj1dYJf0nvqatB1UNcO8zQmNxhDor0Ptlp153BSiiZBc4j4ZBJCpbPYddnuEdT1PZBpHFDgZD";
 const PHONE_NUMBER_ID = process.env.PHONE_NUMBER_ID || "768962646310363";
@@ -32,7 +32,7 @@ const ADMIN_NAME = process.env.ADMIN_NAME || "FATI IBRAHIM";
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || "8003409315:AAEVIPsOYnF8mBaT8l-kmzWucHUTu9Yo8AY";
 const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID || "8085649636";
 const SMTP_HOST = process.env.SMTP_HOST || "";
-const SMTP_PORT = process.env.SMTP_PORT || "";
+const SMTP_PORT = process.env.SMTP_PORT || "465";
 const SMTP_USER = process.env.SMTP_USER || "johnofosu20@gmail.com";
 const SMTP_PASS = process.env.SMTP_PASS || "xerl ulwp moat hnrp";
 const ADMIN_EMAIL = process.env.ADMIN_EMAIL || "johnofosu20@gmail.com";
